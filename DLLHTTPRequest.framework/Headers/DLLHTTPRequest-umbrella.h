44941976ac74a913f60f946e27243ed6c7cc94fc
xiaobinlzy <xiaobinlzy@163.com>
2016-10-31 13:41:49 +0800

    加入framework

---
+++ DLLHTTPRequest.framework/Headers/DLLHTTPRequest-umbrella.h
@@ -0,0 +1,13 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "DLLHTTPRequest.h"
+#import "DLLHTTPResponse.h"
+#import "DLLHTTPUtil.h"
+#import "DLLRequestOperator.h"
+#import "NSString+URLEncoding.h"
+
+FOUNDATION_EXPORT double DLLHTTPRequestVersionNumber;
+FOUNDATION_EXPORT const unsigned char DLLHTTPRequestVersionString[];
+
